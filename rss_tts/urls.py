358a35efa904f59a4abdba2a51419ed6e892bd9b
JM Addington <jonathan.addington@jmaddington.com>
2025-05-21 12:55:25 -0500

    feat: add article submission and auth views

--- rss_tts/urls.py
+++ rss_tts/urls.py
@@ -2,10 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
-from text_to_audio.views import HomeView
+from text_to_audio.views import HomeView, ArticleCreateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("articles/submit/", ArticleCreateView.as_view(), name="article-submit"),
     path("", HomeView.as_view(), name="home"),
 ]
