74d7f052bb936f20ad387599283362d30b1aa0ff
JM Addington <jonathan.addington@jmaddington.com>
2025-05-21 13:41:45 -0500

    chore: ignore media output

--- text_to_audio/views.py
+++ text_to_audio/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import CreateView, TemplateView
 
 from .forms import ArticleSubmissionForm
 from .models import Feed
+from .tasks import process_article
 
 
 class HomeView(TemplateView):
@@ -20,10 +21,9 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy("home")
 
     def form_valid(self, form):
-        feed, _ = Feed.objects.get_or_create(
-            user=self.request.user, name="Default"
-        )
+        feed, _ = Feed.objects.get_or_create(user=self.request.user, name="Default")
         article = form.save(commit=False)
         article.feed = feed
         article.save()
+        process_article.delay(article.id)
         return super().form_valid(form)
