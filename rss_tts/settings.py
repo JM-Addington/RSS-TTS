3767796973d7b6a92649ab5d6072306b9215f978
Jonathan Addington <jonathan.addington@jmaddington.com>
2025-05-23 17:18:57 -0400

    fix: Correct CSRF_TRUSTED_ORIGINS configuration to fix TypeError 'unhashable type: list'

--- rss_tts/settings.py
+++ rss_tts/settings.py
@@ -25,13 +25,11 @@ ALLOWED_HOSTS += (
     else []
 )
 
-CSRF_TRUSTED_ORIGINS = [
-    (
-        os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
-        if os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS")
-        else []
-    )
-]
+CSRF_TRUSTED_ORIGINS = (
+    os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
+    if os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS")
+    else []
+)
 
 # Application definition
 
