2a2e7227ad88cfbe3253f369cc849f12f02d4668
Jonathan Addington <jonathan.addington@jmaddington.com>
2025-05-23 23:46:24 -0400

    fix: Update article ID accessors to use 'pk' for consistency in tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,16 +1,15 @@
 """Tests for the text_to_audio app views."""
 
-# mypy: disable-error-code="attr-defined"
+# mypy: ignore-errors
 
-from unittest import mock
 import uuid
+from unittest import mock
 
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
 from text_to_audio.models import Article, Feed
-from text_to_audio.tasks import process_article
 
 User = get_user_model()
 
@@ -36,38 +35,38 @@ class RegenerateArticleViewTest(TestCase):
 
     @mock.patch("text_to_audio.views.process_article.delay")
     def test_regenerate_article(self, mock_process_article):
-        """Test that regenerating an article creates a new article and queues processing."""
+        """Test regenerating an article creates a new article and queues processing."""
         # Get the initial article count
         initial_count = Article.objects.count()
 
         # Make the post request to regenerate
         response = self.client.post(
-            reverse("article-regenerate", kwargs={"article_id": self.article.id})
+            reverse("article-regenerate", kwargs={"article_id": self.article.pk})
         )
 
         # Check that we were redirected to the feed articles page
         self.assertRedirects(
-            response, reverse("feed-articles", kwargs={"feed_id": self.feed.id})
+            response, reverse("feed-articles", kwargs={"feed_id": self.feed.pk})
         )
 
         # Check that a new article was created
         self.assertEqual(Article.objects.count(), initial_count + 1)
 
         # Get the new article
-        new_article = Article.objects.exclude(id=self.article.id).first()
-        
+        new_article = Article.objects.exclude(pk=self.article.pk).first()
+
         # Check that the new article has the correct properties
         self.assertEqual(new_article.feed, self.article.feed)
         self.assertEqual(new_article.title, self.article.title)
         self.assertEqual(new_article.text_content, self.article.text_content)
         self.assertEqual(new_article.status, Article.PROCESSING)
-        
+
         # Check that the new article has a different UUID
         self.assertIsNotNone(new_article.audio_uuid)
         self.assertNotEqual(new_article.audio_uuid, self.article.audio_uuid)
-        
+
         # Check that process_article.delay was called with the new article's ID
-        mock_process_article.assert_called_once_with(new_article.id)
+        mock_process_article.assert_called_once_with(new_article.pk)
 
     def test_regenerate_article_access_control(self):
         """Test that users can only regenerate their own articles."""
@@ -86,9 +85,9 @@ class RegenerateArticleViewTest(TestCase):
 
         # Try to regenerate the other user's article
         response = self.client.post(
-            reverse("article-regenerate", kwargs={"article_id": other_article.id})
+            reverse("article-regenerate", kwargs={"article_id": other_article.pk})
         )
-        
+
         # Should return 404 since the user doesn't own the article
         self.assertEqual(response.status_code, 404)
 
